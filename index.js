eb5a46e0e4bc65fdf91d53c99a424b7fed20b720
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-11-28 16:40:56 +0900

    feat:index 추가 수정

--- index.js
+++ index.js
@@ -27,15 +27,31 @@ console.log(process.env.VITE_USER);
 console.log(process.env.VITE_PASSWORD);
 console.log(process.env.VITE_DATABASE);
 
+// CORS 프리플라이트 요청 처리
+app.options('*', cors());
+
 // 미들웨어
 app.use(
   cors({
-    origin: ['https://javisproject.duckdns.org'],
+    origin: 'https://javisproject.duckdns.org',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    exposedHeaders: ['Set-Cookie'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   })
 );
+
+// CORS 헤더 추가 미들웨어
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://javisproject.duckdns.org');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 // 라우터 등록
