0022cb089ca5375ded25990916a5d9b3899ebced
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-11-29 09:15:01 +0900

    feat:배포용 수정

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql2");
 var connection = mysql.createConnection({
-  host: process.env.VITE_HOST, // DuckDNS 도메인만
+  host: import.meta.env.DEV ? process.env.VITE_HOST: "http://localhost",
   port: process.env.VITE_PORT, // 포트를 별도로 지정
   user: process.env.VITE_USER,
   password: process.env.VITE_PASSWORD,
